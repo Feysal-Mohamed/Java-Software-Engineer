81b9c640b84451febe9186ff058924d4b40f3117
Feysal_Mohamed <feizalabdi@gmail.com>
2026-08-26 11:58:21 +0300

    jksk:

--- day_1/src/BankAccount.java
+++ day_1/src/BankAccount.java
@@ -14,5 +14,23 @@ class  Bank{
 
 public class BankAccount {
 
+    public static void main(String[] args) {
+        Bank owner1 = new Bank();
+        Bank owner2 = new Bank();
 
+        owner1.acountId=1;
+        owner1.OwnerName ="Feysal";
+        owner1.Balance =1000;
+        owner1.acountId=2;
+        owner2.OwnerName ="Maxamed";
+        owner2.Balance =500;
+
+        owner1.DisplayInfo();
+        owner2.DisplayInfo();
+
+        owner1.Deposite(200);
+        owner2.WithDraw(100);
+
+
+    }
 }
