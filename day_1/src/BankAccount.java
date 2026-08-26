bf10bbbe12cde3d83ce47addde1e5ac910d7f8db
Feysal_Mohamed <feizalabdi@gmail.com>
2026-08-26 11:57:49 +0300

    hsk

---
+++ day_1/src/BankAccount.java
@@ -0,0 +1,18 @@
+
+class  Bank{
+    int acountId;
+    String OwnerName ;
+    double Balance;
+
+    void DisplayInfo(){
+        System.out.println("account "+acountId);
+        System.out.println("owner : "+OwnerName);
+        System.out.println("Balance is  : $"+Balance);
+    }
+
+}
+
+public class BankAccount {
+
+
+}
