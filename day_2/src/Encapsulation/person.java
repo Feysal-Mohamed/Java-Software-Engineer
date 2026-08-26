957e2cbbe285f1ba24deab139ce8e6e259524fb1
Feysal_Mohamed <feizalabdi@gmail.com>
2026-08-26 13:42:10 +0300

    person class comleted

--- day_2/src/Encapsulation/person.java
+++ day_2/src/Encapsulation/person.java
@@ -11,9 +11,28 @@ class Presons{
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+       if(age>0){
+           this.age = age;
+       }else {
+           System.out.println(age +" age must greater than 0");
+       }
+    }
 }
 public class person {
 
+    public static void main(String[] args) {
+        Presons p1= new Presons();
+        p1.setName("feysal");
+        p1.setAge(-9);
 
+
+
+        System.out.println(p1.getName());
+        System.out.println(p1.getAge());
+    }
 }
